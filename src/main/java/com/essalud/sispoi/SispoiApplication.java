09a8836cd04b40024718c8bffc95841e79fdd120
Jhonathan Centti <centtijhonathan@gmail.com>
2025-06-18 17:32:45 +0000

    Initial commit

---
+++ src/main/java/com/essalud/sispoi/SispoiApplication.java
@@ -0,0 +1,13 @@
+package com.essalud.sispoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SispoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/essalud/sispoi/SispoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essalud.sispoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SispoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
